e638b0650be28825a5e2ef9d475ca3b2c01b2207
Daniel Nissan <dniss001@wch136-24.cs.ucr.edu>
2021-04-10 19:37:58 -0700

    2.2

--- Lab2_introToAVR/turnin/dniss001_lab2_part2.c
+++ Lab2_introToAVR/turnin/dniss001_lab2_part2.c
@@ -7,35 +7,37 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 	//unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-	//unsigned char tmpA = 0x00; // Temp var to hold value of A
-	unsigned char pa3 = 0x00; // garage door sensos (1 = door open)
-        unsigned char pa2 = 0x00; // light sensor (1 = light sensed)
-	unsigned char pa1 = 0x00; // garage door sensos (1 = door open)
-	unsigned char pa0 = 0x00; // light sensor (1 = light sensed)
+	unsigned char tmpA = 0x00; // Temp var to hold value of A
+	//unsigned char pa3 = 0x00; // garage door sensos (1 = door open)
+        //unsigned char pa2 = 0x00; // light sensor (1 = light sensed)
+	//unsigned char pa1 = 0x00; // garage door sensos (1 = door open)
+	//unsigned char pa0 = 0x00; // light sensor (1 = light sensed)
 
 	unsigned char cnt;
 while(1) {
 		// 1) Read input
-		//tmpA = PINA & 0x01;
-		pa0 = PINA  & 0x01;
+		tmpA = PINA;
+		/*pa0 = PINA  & 0x01;
 		pa1 = (PINA >> 1) & 0x01;
 		pa2 = (PINA >> 2) & 0x01;
-		pa3 = (PINA >> 3) & 0x01;
+		pa3 = (PINA >> 3) & 0x01;*/
 
 		// 2) Perform computation
-		if (pa0 == 0x01 ) {
-			cnt++;
+		if (tmpA == 0x00 ) {
+			cnt = 0x04;
 		}
-		if (pa1 == 0x01 ) {
-                        cnt++;
+		else if (tmpA == 0x01 || tmpA == 0x02 || tmpA == 0x04 || tmpA == 0x08 ) {
+                        cnt = 0x03;
                 }	
-		if (pa2 == 0x01 ) {
-                        cnt++;
-                }
-		if (pa3 == 0x01 ) {
-                        cnt++;
-                }
-
+		else if (tmpA == 0x03 || tmpA == 0x05 || tmpA == 0x06 || tmpA == 0x09 || tmpA == 0x0A || tmpA == 0x0C) {
+			cnt = 0x02;
+		}
+		else if (tmpA == 0x07 || tmpA == 0x0B || tmpA == 0x0D || tmpA == 0x0E) {
+                	cnt = 0x01;
+        	}
+		else {
+			cnt = 0x00;
+		}
 		// 3) Write output
 		PORTC = cnt;	
 	}
