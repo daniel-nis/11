f548bfee93988ceac361d2b7f2739ce003212b43
Daniel Nissan <dniss001@wch136-24.cs.ucr.edu>
2021-04-10 20:42:39 -0700

    part 3

---
+++ Lab2_introToAVR/turnin/dniss001_lab2_part3_tests.py
@@ -0,0 +1,23 @@
+tests = [ {'description': 'PINA: 0x00 => PORTC: 0x04',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x04)],
+    },
+
+    {'description': 'PINA: 0x01 => PORTC: 0x03',
+    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x03)],
+    },
+
+    {'description': 'PINA: 0x03 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+
+    {'description': 'PINA: 0x0F => PORTC: 0xB0',
+    'steps': [ {'inputs': [('PINA',0x0F)], 'iterations': 5 } ],
+    'expected': [('PORTC',0xB0)],
+    },
+
+ 
+    ]
+#watch = ['PORTB']
